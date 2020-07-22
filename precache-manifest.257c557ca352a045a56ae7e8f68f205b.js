988f2bdb199cc60ff5f50cb6ee90b5b4174c267c
Daniel Yap <danielytf@gmail.com>
2020-07-22 17:45:57 +1000

    Updates

---
+++ precache-manifest.257c557ca352a045a56ae7e8f68f205b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9fd3a316a2b92f8a537faa92d9a80f",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "1df12b2f5fe8872931e9",
+    "url": "/my-react-app/static/css/2.4c093531.chunk.css"
+  },
+  {
+    "revision": "292447793da107281b1d",
+    "url": "/my-react-app/static/css/main.63e8db95.chunk.css"
+  },
+  {
+    "revision": "1df12b2f5fe8872931e9",
+    "url": "/my-react-app/static/js/2.4d4e4f5e.chunk.js"
+  },
+  {
+    "revision": "23692ada4101600d3769adeb08781fb7",
+    "url": "/my-react-app/static/js/2.4d4e4f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292447793da107281b1d",
+    "url": "/my-react-app/static/js/main.528c4b0a.chunk.js"
+  },
+  {
+    "revision": "aaa6c7d46042ee91d40b",
+    "url": "/my-react-app/static/js/runtime-main.c7b85f79.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/my-react-app/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/my-react-app/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/my-react-app/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/my-react-app/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/my-react-app/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
