d77af0bf9d6ac93d4ee612dcc182c818bf71e06a
Daniel Yap <danielytf@gmail.com>
2020-10-18 13:16:52 +1100

    Update window.js

--- src/util/window.js
+++ src/util/window.js
@@ -1,6 +1,7 @@
 const decrypt = (t) => {
-  if (t === null || t === undefined || t === "") return t;
-  return t.slice(1, -1);
+  return t;
+  //if (t === null || t === undefined || t === "") return t;
+  //return t.slice(1, -1);
 };
 window.app = {};
 window.app.decrypt = decrypt;
